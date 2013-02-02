2cc799723834c2d3b60d1e7fe31b1b03c87c56b2
Seth Faxon <seth.faxon@gmail.com>
2013-02-02 10:47:02 -0800

    LineItem parses description

--- lib/cli_shopping/line_item.rb
+++ lib/cli_shopping/line_item.rb
@@ -1,9 +1,10 @@
 class LineItem
   attr_accessor :count, :description, :price
   def initialize(cli_input)
-    m = /(?<count>\d+)/.match(cli_input)
+    m = /(?<count>\d+)\s+(?<description>.*)(\s+)(at)/.match(cli_input)
     if m
       @count = m[:count].to_i
+      @description = m[:description]
     end
   end
 end
--- spec/models/line_item_spec.rb
+++ spec/models/line_item_spec.rb
@@ -7,10 +7,20 @@ describe LineItem do
         line_item = LineItem.new("1 book at 12.49")
         line_item.count.should eq(1)
       end
-    
       it "should find multiple digits" do
         line_item = LineItem.new("100 boxes of chocolate at 12.49")
         line_item.count.should eq(100)
+      end
+    end
+
+    context "description" do
+      it "should match single word" do
+        line_item = LineItem.new("1 book at 12.49")
+        line_item.description.should eq("book")
+      end
+      it "should match multiple words" do
+        line_item = LineItem.new("1 bottle of perfume at 18.99")
+        line_item.description.should eq("bottle of perfume")
       end
     end
   end
