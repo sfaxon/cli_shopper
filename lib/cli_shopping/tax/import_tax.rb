68e7ba11c031307d759f0689b8b3704bfd7bfce6
Seth Faxon <seth.faxon@gmail.com>
2013-02-02 16:50:41 -0800

    refactor ImportTax to use TAX_RATE constant

--- lib/cli_shopping/tax/import_tax.rb
+++ lib/cli_shopping/tax/import_tax.rb
@@ -1,5 +1,6 @@
 class ImportTax < Tax
   DESCRIPTION_KEYWORDS = %w(imported)
+  TAX_RATE = 0.05
   class << self
     def applies_to?(line_item)
       DESCRIPTION_KEYWORDS.each do |keyword|
@@ -8,9 +9,10 @@ class ImportTax < Tax
       false
     end
     
+    # returns float of the rounded tax ImportTax amount on the item
     def on(line_item)
-      return 0 if !applies_to?(line_item)
-      return line_item.price * 0.05
+      return 0.0 if !applies_to?(line_item)
+      calculate_rounded_tax(line_item.price, TAX_RATE)
     end
   end
 end
